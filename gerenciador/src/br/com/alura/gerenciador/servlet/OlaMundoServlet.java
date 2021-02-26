47e9c91ea4dd6f4e137795537fbb711cc181d385
Gabriel <g.dessilva95@gmail.com>
2021-02-25 22:55:39 -0300

    01 - Fundamentos da Web e a API de Servlets

---
+++ gerenciador/src/br/com/alura/gerenciador/servlet/OlaMundoServlet.java
@@ -0,0 +1,26 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/hello")
+public class OlaMundoServlet extends HttpServlet{
+	
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws IOException{
+		
+		PrintWriter out =  res.getWriter();
+		out.print("<html>");
+		out.print("<body>");
+		out.print("Ola mundo, parabens vc escreveu o primeiro servlets");	
+		out.print("</body>");
+		out.print("</html>");
+		
+	}
+	
+}
